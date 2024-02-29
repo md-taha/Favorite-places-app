dadb7eacdf2348797e14230cd59609c510fd5f31
md-taha <mohammadtaha9935@gmail.com>
2024-02-29 09:18:54 +0530

    Added raw template of ImageInput widget.

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,1 +1,29 @@
-class ImageInput extends statelesswidget
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: () {},
+        icon: const Icon(Icons.camera),
+        label: const Text("Take picture"),
+      ),
+    );
+  }
+}
